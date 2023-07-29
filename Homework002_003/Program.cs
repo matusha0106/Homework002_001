5e532dd8443a788f2203b92724d28d1b29b736af
Anastasiya <podik0106@icloud.com>
2023-07-29 16:26:37 +0300

    Find day week

---
+++ Homework002_003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите номер дня недели "); 
+                int num = int.Parse (Console.ReadLine()); 
+                if (num >= 1 & num <= 5) //& = и
+                { 
+                    Console.Write("Это не выходной"); 
+                } 
+ 
+                else if (num >= 6 & num <= 7) 
+                { 
+                    Console.Write(" Это выходной"); 
+                } 
+                else Console.Write(" Не существует такого дня недели");
