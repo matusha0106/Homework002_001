daaa4e4a186c81444b0c6f8d3361a54ccc5dd00c
Anastasiya <podik0106@icloud.com>
2023-07-29 16:11:26 +0300

    finde three number

---
+++ Homework002_002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+        int.TryParse(Console.ReadLine()!, out int a);
+    
+        if (a>999 || a<100)
+        {
+        Console.WriteLine("Не трехзначное число");
+        return;
+        }
+        int GetSecond(int a)
+        {
+        while (a >= 1000) a /= 10;
+        int b = a % 10;
+        return b;
+        }
+        Console.WriteLine(GetSecond(a));
